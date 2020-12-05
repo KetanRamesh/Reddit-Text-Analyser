b1d40489041fcae9c5fe109561cb7aa85a092c1c
KetanRamesh <ketanramesh22@gmail.com>
2020-12-05 05:12:33 +0000

    Updated sentiment

--- app/sentiment.py
+++ app/sentiment.py
@@ -8,6 +8,8 @@ import datetime as dt
 
 import praw
 from praw.models import MoreComments
+from detoxify import Detoxify
+
 import pika
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -24,6 +26,7 @@ class Sentiment:
         self.reddit = None
         self.submissions = {}
 
+    ## Remove in final version
     def connectReddit(self):
         try:
             self.reddit = praw.Reddit(
@@ -35,6 +38,7 @@ class Sentiment:
         except:
             print("Error connecting to Reddit.")
 
+    ## Replace with Firebase retrieval code
     def getSubmissions(self):
         self.submissions = {
             "id": [],
@@ -53,53 +57,12 @@ class Sentiment:
 
         self.submissions = pd.DataFrame(self.submissions)
 
-    def expandComments(self, morecomments):
-        comments = morecomments.comments()
-        if isinstance(comments, MoreComments):
-            return self.expandComments(comments)
-        
-        return comments
-
-    def getCommentsTree(self, morecomments):
-        comments = []
-        count = 5
-        for comment in morecomments:
-            if (count == 0):
-                break
-            post = self.reddit.comment(comment.id)
-            comments.append(post.body)
-            count -= 1
-        
-        return comments
-
-    def getComments(self):
-        self.comments = {}
-
-        for i in range(len(self.submissions)):
-            post = self.submissions.loc[i]
-            submission = self.reddit.submission(id=post['id'])
-            submission.comment_limit = 3
-            top_comments = submission.comments
-            for comment in top_comments:
-                if isinstance(comment, MoreComments):
-                    comments = self.expandComments(comment)
-                    comments = self.getCommentsTree(comments)
-                    self.comments[post['id']] = comments
-                    continue
-                if post['id'] in self.comments:
-                    self.comments[post['id']].append(comment.body)
-                else:
-                    self.comments[post['id']] = []
-                    self.comments[post['id']].append(comment.body)
-
-    def addComments(self):
-
-        for ind in range(len(self.submissions['id'])):
-            post_id = self.submissions['id'][ind]
-            comments = self.comments[post_id]
-            self.submissions['comments'] = comments
-
     def computeSentiment(self):
+        senti_count = {
+            "positive": 0,
+            "negative": 0,
+            "neutral": 0
+        }
         sia = SentimentIntensityAnalyzer()
         sentiments = []
 
@@ -110,11 +73,41 @@ class Sentiment:
             score['headline'] = text
             sentiments.append(score)
 
+        for sentiment in sentiments:
+            if not sentiment['neg'] > 0.05:
+                if sentiment['pos'] - sentiment['neg'] > 0:
+                    senti_count['positive'] += 1
+                else:
+                    senti_count['neutral'] += 1
+            elif not sentiment['pos'] > 0.05:
+                if sentiment['pos'] - sentiment['neg'] <= 0:
+                    senti_count['negative'] += 1
+                else:
+                    senti_count['neutral'] += 1
+            else:
+                senti_count['neutral'] += 1
+        
+        print(senti_count)
+
+    def getToxicity(self):
+        toxic_count = {
+            "very_toxic": 0,
+            "toxic": 0,
+            "hard_to_say": 0,
+            "not_toxic": 0
+        }
+
+        for i in range(len(self.submissions)):
+            post = self.submissions.loc[i]
+            text = post['title']
+            label = Detoxify('original').predict(text)
+            print(label)
+
     def worker(self):
         self.connectReddit()
         self.getSubmissions()
-        self.getComments()
         self.computeSentiment()
+        # self.getToxicity()
 
 def getRabbitMQ():
     try:
@@ -150,7 +143,7 @@ if __name__ == "__main__":
     # main()
     auth_file = '../auth.json'
     sub = 'learnpython'
-    limit = 3
+    limit = 100
     
     with open(auth_file) as auth_param:
         param = json.load(auth_param)
